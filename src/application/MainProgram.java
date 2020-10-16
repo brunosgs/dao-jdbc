a1400363afd402c89e12b84a853e1673c17ce76d
Bruno Gomes <brunosoges@gmail.com>
2020-10-15 22:54:16 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -10,7 +10,7 @@ public class MainProgram {
 	public static void main(String[] args) {
 		Department department = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
-		
+
 		System.out.println(department);
 		System.out.println(seller);
 	}
